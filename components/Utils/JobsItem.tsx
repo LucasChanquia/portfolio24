c48bd274dcd48fe31ad34ef653a9aa08b4faf62e
LucasChanquia <lucaschanquia@gmail.com>
2024-01-19 02:39:16 -0300

    docs: :art: Arrays
    Arrays de información necesaria

---
+++ components/Utils/JobsItem.tsx
@@ -0,0 +1,20 @@
+export const jobs = [
+    {
+        id: 1,
+        name: 'SETA',
+        image: ['/image/SETA/uno.jpg', '/image/SETA/dos.jpg', '/image/SETA/tres.jpg', '/image/SETA/cuatro.jpg', '/image/SETA/cinco.jpg'],
+        description: 'SETA is a custom-made app for a car wash in Mar del Plata, Buenos Aires. In this project, my role was to handle all frontend production, emphasizing user experience. Relevant tasks included setting up filters for brand, model, and age of a vehicle, as well as creating a step-by-step order process when a vehicle enters the car wash, login, credential management, tariff modifications, among others. My greatest achievement was delivering a fully functional application, with error handling and 100% tested to minimize any chance of bugs. I am currently providing ongoing support to the system owners.',
+        skill: ['NextJs', 'Typescript', 'Taildwind', 'HTML', 'Figma'],
+        url: 'https://seta-web-app.vercel.app/'
+    },
+    {
+        id: 2,
+        name: 'Findink',
+        image: ['/image/Findink/uno.png', '/image/Findink/dos.png', '/image/Findink/tres.png', '/image/Findink/cuatro.png'],
+        description:'Findink is a startup focused on connecting tattoo artists with clients. In the project, it was my responsibility to refactor the login (sign-up & register) and forgot password functionalities. These components were implemented with a validator for each input, resulting in a significant improvement in the user experience.',
+        skill: ['NextJs', 'Typescript', 'Sass', 'HTML', 'Figma'],
+        url:'https://www.findink.co/'
+    },
+
+]
+
